407fe864b8cda78629d71ae3abd5a165021ae9b4
Christer <olofsson.christer@outlook.com>
2025-11-22 13:39:23 +0100

    Mer innehåll

--- fil1.py
+++ fil1.py
@@ -1,1 +1,15 @@
 print("Hej Christer!   ")
+
+namn="Christer"
+ålder=79
+print("Hej", namn, "Du är", ålder, "år gammal.")    
+
+färg="blå"
+nummer=7
+print("Din favoritfärg är", färg)
+print("Ditt favoritnummer är", nummer)
+
+tal1=15
+tal2=4
+summa=tal1+tal2
+print("Summan av", tal1, "och", tal2, "är", summa)
