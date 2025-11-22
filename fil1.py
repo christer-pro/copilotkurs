fdd4f4174087bab6aa4b92359dfc75db839ef5c9
Christer <olofsson.christer@outlook.com>
2025-11-22 15:55:13 +0100

    Funktioner

--- fil1.py
+++ fil1.py
@@ -13,3 +13,38 @@ tal1=15
 tal2=4
 summa=tal1+tal2
 print("Summan av", tal1, "och", tal2, "är", summa)
+
+förnamn="Christer"
+efternamn="Olofsson"
+print(förnamn + " " + efternamn)
+print("Hej,", förnamn, efternamn)
+
+text="python är kul"
+
+print(text.upper())
+print(text.capitalize())
+print(text.replace("kul", "roligt"))
+print("Längden på texten är", len(text))
+print("Finns 'är' i texten?", "är" in text)
+print(text.lower())
+print(text.title())
+print(text.count("o"))
+print(text.split())
+print(text.find("är"))
+print(text.startswith("python"))
+print(text.endswith("kul"))
+print(text[::-1])
+print(text.center(30, "-"))
+print(text.swapcase())
+print(text.strip())
+print(text.index("kul"))
+print(text.isalpha())
+print(text.islower())
+print(text.isupper())
+print(text.zfill(20))
+print("Byt ut 'python' mot 'Java':", text.replace("python", "Java"))
+
+
+print(förnamn.upper())
+print(förnamn.)
+print(efternamn.lower())
--- fil2.py
+++ fil2.py
@@ -3,4 +3,15 @@
 if ålder >= 18:
     print("Du är myndig.")
 else:
-    print("Du är inte myndig.")
+    print("Du är inte myndig.") 
+
+for i in range(5):
+    print("Räkna:", i + 1)                                    
+
+nummer=1
+while nummer <= 5:
+    print("Nummer är:", nummer)
+    nummer += 1                          
+
+for i in range(10):
+    print(i+1)
