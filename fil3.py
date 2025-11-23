1626133dc7a6ea5795f930cd443a5f22c0800003
Christer <olofsson.christer@outlook.com>
2025-11-24 00:20:33 +0100

    uppdaterad

--- fil3.py
+++ fil3.py
@@ -4,3 +4,18 @@ def hälsa(namn):
 
 hälsa("Christer")
 hälsa("Anna")
+
+def nummer(tal):
+    if tal>10:
+        print(tal,"är störe än 10")
+    
+    elif tal==10:
+        print(tal,"är lika med 10")
+    
+    else:
+        print(tal,"är mindre än 10")     
+    
+
+nummer(2)
+nummer(15)  
+nummer(10)
